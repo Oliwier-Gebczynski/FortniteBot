89a77cd1afa628dd5b56af460a8787d48723a06a
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2022-04-18 00:41:50 +0200

    first day with new api

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 import discord
+import requests
+import key
+import json
 
-token = ""
+token = key.TOKEN
+api_key = key.API
+print(api_key)
+
+headers = {
+        'Authorization': api_key
+}
+
 print("Logging in...")
 
 client = discord.Client()
 
-songList = []
+def get_store(headers):
+    url = f"https://fortnite-api.theapinetwork.com/store/get"
+    payload = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return response.text
 
 @client.event
 async def on_ready():
@@ -13,18 +28,21 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('/Hello Cloudy'):
+    if message.content.startswith('/Hello'):
         await message.channel.send('Hello there!')
 
     elif message.content.startswith('/help'):
-        await message.channel.send('1. If you want add song try this "/p linkFromSoundCloud" . 2. Docs: https://github.com/Oliwier-Gebczynski/SoundCloudBot')
+        await message.channel.send("1. Commands: /p_info, /m_history, /store, /challenges. 2. Docs: https://github.com/Oliwier-Gebczynski/SoundCloudBot")
 
-    elif message.content.startswith('/playlist'):
-        await message.channel.send(songList)
+    elif message.content.startswith('/store'):
+        store = get_store(headers)
+        print(store)
+        await message.channel.send(store)
 
     elif message.content.startswith('/p'):
-        songList.append(message.content[3:])
         await message.channel.send(f"Song {message.content[3:]} added to playlist!")
-        print(songList)
+
+
+
 
 client.run(token)
